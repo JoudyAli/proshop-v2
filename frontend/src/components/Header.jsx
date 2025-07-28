e236097bdb715864e625796069ce40403362e960
Joudy <joudyali.de@gmail.com>
2025-07-28 02:16:27 +0200

    Fetch Products

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,7 @@
 import { Navbar,Nav,Container } from "react-bootstrap";
 import { FaShoppingCart,FaUser  } from "react-icons/fa";
 import { LinkContainer } from "react-router-bootstrap";
-import logo from '../assets/logo.png'; // Assuming you have a logo image";
+import logo from '../assets/logo.png';
 
 const Header = () => {
   return (
