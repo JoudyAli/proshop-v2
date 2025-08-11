e0653e6313fd89daf4329b7d6c86e36cc7b26aaf
Joudy <joudyali.de@gmail.com>
2025-08-11 23:17:52 +0200

    User Profile Endpoints

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -15,7 +15,7 @@ import asyncHandler from '../middleware/asyncHandler.js';
   if (user && (await user.matchPassword(password))){
   generateToken(res,user._id);
 
-   res.json({
+   res.status(200).json({
       _id : user._id,
       name : user.name,
       email : user.email,
@@ -51,7 +51,7 @@ import asyncHandler from '../middleware/asyncHandler.js';
 
     if (user){
       generateToken(res,user._id);
-      
+
       res.status(201).json({
          _id : user._id,
          name : user.name,
@@ -82,7 +82,19 @@ import asyncHandler from '../middleware/asyncHandler.js';
   // @access  Private
 
  const getUserProfile=asyncHandler(async(req,res)=>{
-    res.send(' Get User profile ')
+    const user = await User.findById(req.user._id);
+    if (user) {
+      res.status(200).json({
+           _id : user._id,
+         name : user.name,
+         email : user.email,
+         isAdmin: user.isAdmin
+      });
+      
+    } else {
+       res.status(400);
+      throw new Error('User not found');
+    }
  });
 
 
@@ -91,7 +103,29 @@ import asyncHandler from '../middleware/asyncHandler.js';
   // @access  Public
 
  const updateUserProfile=asyncHandler(async(req,res)=>{
-    res.send('  update Get User profile ')
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+      user.name= req.body.name || user.name;
+      user.email= req.body.email || user.email;
+      
+      if (req.body.password){
+         user.password=req.body.password;
+      }
+
+      const updatedUser = await user.save();
+      res.status(200).json({
+          _id : updatedUser._id,
+         name : updatedUser.name,
+         email : updatedUser.email,
+         isAdmin: updatedUser.isAdmin
+      });
+
+      
+    } else {
+          res.status(400);
+      throw new Error('User not found');
+    }
  });
 
 
