70ab10713569e7bf13b7c7b72c360eed9945bf34
Joudy <joudyali.de@gmail.com>
2025-08-12 12:35:46 +0200

    Auth & User Api Slice

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,12 +2,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { apiSlice } from './slices/apiSlice'; // Import the API slice
 import cartSliceReducer from './slices/cartSlice'; // Import the cart slice
+import authSliceReducer from './slices/authSlice';
 
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer, // Add the API slice reducer 
     cart: cartSliceReducer, // Add the cart slice reducer 
+    auth: authSliceReducer,
   },
 middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware), // Add the API slice middleware
   devTools:true, // Enable Redux DevTools
