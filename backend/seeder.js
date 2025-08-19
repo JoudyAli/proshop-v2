620be60a6836d2a6a08e14b0206a991ec563892b
Joudy <joudyali.de@gmail.com>
2025-08-19 16:36:09 +0200

    Create & Get Orders

--- backend/seeder.js
+++ backend/seeder.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import colors from "colors";
+
 import user from "./data/users.js";
 import products from "./data/products.js";
 import User from "./models/userModel.js";
