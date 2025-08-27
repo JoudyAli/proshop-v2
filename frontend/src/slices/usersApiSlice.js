3de2cc203b420eefc0c340aa2524490b6d392d82
Joudy <joudyali.de@gmail.com>
2025-08-27 17:40:19 +0200

    Update Users

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -45,9 +45,23 @@ export const usersApiSlice = apiSlice.injectEndpoints({
             }),
             
          }),
-
-        }),
-     });
+ 
+          getUserDetails : builder.query({
+            query : (userId) =>({
+             url : `${USERS_URL}/${userId}`,
+            }),
+            keepUnusedDataFor : 5
+           }),
+           updateUser : builder.mutation({
+            query : ({data}) =>({
+             url : `${USERS_URL}/${data.userId}`,
+             method : 'PUT',
+             body : data,
+            }),
+            invalidatesTags : ['User']
+         }),
+    }),
+ });
 
 export const { 
    useLoginMutation,
@@ -56,6 +70,8 @@ export const {
    useProfileMutation,
    useGetUsersQuery,
    useDeleteUserMutation,
+   useGetUserDetailsQuery,
+   useUpdateUserMutation,
  } = usersApiSlice;
  // Export the hook for use in components
 
