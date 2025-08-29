964d1b9e366febde89e679e2907027d8d019608a
Joudy <joudyali.de@gmail.com>
2025-08-29 23:18:30 +0200

    Paginate Products

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,11 +1,14 @@
 import { Row,Col } from "react-bootstrap"
+import { useParams } from "react-router-dom";
 import Product from '../components/Product';
 import Loader from "../components/Loader";
 import Message from "../components/Message";
 import { useGetProductsQuery } from "../slices/productsApiSlice"; // Import the hook to fetch products
 
 const HomeScreen = () => {
-   const { data: products , isLoading, error } = useGetProductsQuery(); // Use the hook to get products
+
+  const {pageNumber}=useParams();
+   const { data , isLoading, error } = useGetProductsQuery({pageNumber}); // Use the hook to get products
   return (
     <>
     {isLoading ?(
@@ -13,7 +16,7 @@ const HomeScreen = () => {
     ): error ? ( <Message variant='danger'>{error?.data?.message || error.error} </Message>):(<>
     <h1>List Products</h1>
       <Row>
-        {products.map((products)=>(
+        {data.products.map((products)=>(
                     <Col key={products._id} sm={12} md={6} lg={4} xl={3} >
                 <Product product={products} />
             </Col>
