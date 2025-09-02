ca81835e8e6f114e9acc8233d9093694e45d2903
Joudy <joudyali.de@gmail.com>
2025-09-02 11:43:35 +0200

    Fix wildcard route to prevent path-to-regexp error

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,13 @@ import dotenv from "dotenv"
 import  cookieparser  from 'cookie-parser';
 dotenv.config()
 import connectDB from "./config/db.js"
-import {notfound, errorHandler} from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
+import {notfound, errorHandler} from "./middleware/errorMiddleware.js";
+
+
 const port= process.env.PORT || 5000;
 
 connectDB(); // Connect to MongoDB
@@ -41,10 +43,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')));
 
   //any route that is not api will be redirected to index.html
-  app.get('*', (req, res)=>
-  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  );
-
+    app.get(/^\/(?!api).*/, (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+  });
 }else{
   app.get("/", (req, res) => {
   res.send("API is running...");
